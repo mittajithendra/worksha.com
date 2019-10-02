e462e7a9059773531191bb7219afff3552107de4
mittajithendra <mittajithendra46@gmail.com>
2019-10-02 10:54:38 +0530

    files added

---
+++ src/pages/authors/imthiyaz.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+export default function Problems() {
+    return (
+      <div>
+  <center><h1>Imthiyaz's Profile</h1></center>
+  <h3>Still Pending</h3>
+  <ul>
+        <li>Armstrong Number</li>
+		<li>Shell sort</li>
+	</ul>
+        
+        
+      </div>
+    )
+  }
+  
